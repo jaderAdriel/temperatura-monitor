1d6e8156150a86a193a099fdcc136278078e2a6c
jader <jaderadriel7@gmail.com>
2023-06-26 14:13:21 -0300

    alteraçoes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import History from './pages/History';
 import Statistics from './pages/Statistics';
 import './App.scss'
+import Login from './pages/Login';
 
 function App() {
 
   return (
     <div className='container main-wrapper'>
       <Routes>
+        <Route path='/login'    element={ <Login /> }></Route>
         <Route path='/historico'    element={ <History /> }></Route>
         <Route path='/estatisticas' element={ <Statistics /> }></Route>
       </Routes>
